68bb785171a8439c481631c675bfef37aa7fb84b
Andy <Andymic12@gmail.com>
2014-10-21 14:18:58 -0400

    add FileHandler

--- FlashCards.cpp
+++ FlashCards.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <algorithm>
+#include <iterator>
+#include <fstream>
+#include <sstream>
+#include <vector>
 using namespace std;
 
 class Card{
@@ -13,6 +18,35 @@ public:
 	}
 };
 
+class FileHandler{
+public:
+	const string location;
+
+	FileHandler(string loc=""):location(loc)
+	{
+	}
+	void ReadFile()
+	{
+		string line;
+		ifstream file(location.c_str(), ifstream::in);
+		std::vector<string> vc;
+		if(file.is_open())
+		{
+			while(getline(file,line))
+			{
+				vc.push_back(line);
+			}
+		}
+
+		for(int i=0; i<vc.size(); i++)
+		{
+			cout<<vc[i]<<endl;
+		}
+		file.close();
+	}
+
+
+};
 ostream& operator<<(ostream &out, Card & cCard)
 {
 	out << cCard.word << ": " << cCard.definition << endl;
@@ -25,12 +59,17 @@ istream& operator>>(istream &in, Card & cCard)
 	in>>cCard.definition;
 	return in;
 }
+
+
 int main(void)
 {
 	Card cd;
 	cd.word = "blah";
 	cd.definition = "nope";
 	cout << cd;
+
+	FileHandler fl("");
+	fl.ReadFile();
 
 	
 	return 0;
