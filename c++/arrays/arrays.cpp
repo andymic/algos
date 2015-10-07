4b92055d65fb2d28dda749df7b0d4b529cc83aa8
Andy <Andymic12@gmail.com>
2015-10-06 20:50:37 -0400

    removed maxSubArraySum

--- c++/arrays/arrays.cpp
+++ c++/arrays/arrays.cpp
@@ -209,26 +209,6 @@ void MergeSortTest()
     cout<<endl;
 }
 
-int max(int x, int y)
-{ 
-    return (y > x)? y : x; 
-}
-
-
-int maxSubArraySum(int a[], int size)
-{
-    //Largest Sum Contiguous Subarray
-   int max_so_far = a[0], i;
-   int curr_max = a[0];
- 
-   for (i = 1; i < size; i++)
-   {
-        curr_max = max(a[i], curr_max+a[i]);
-        max_so_far = max(max_so_far, curr_max);
-   }
-   return max_so_far;
-}
-
 int main(void)
 { 
    RotateTest();
