c1cf30b73244d4b82f16f5dc2273c644fd6e6c76
Andy <Andymic12@gmail.com>
2015-10-13 23:38:54 -0400

    added setofstacks

--- c++/stack/stack.cpp
+++ c++/stack/stack.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 class Stack{
@@ -9,6 +9,7 @@ Stack(int x)
 {
 	top = 0;
 	len = x;
+	min = 0;
 	ar = new int[len];
 }
 
@@ -18,6 +19,9 @@ void Push(int x)
 		return;
 
 	ar[top++] = x;
+
+	if(x < ar[min])
+		min = top -1;
 }
 
 int Pop()
@@ -25,33 +29,142 @@ int Pop()
 	if(top==0)
 		return -1;
 
+	if(top - 1 == min)
+	{
+		int currMin = 0;
+		for(int i = 1; i < top - 1; i++)
+		{
+			if(ar[i] < ar[currMin])
+				currMin = i;
+		}
+
+		min = currMin;
+	}
+
 	return ar[--top];
 }
 
+int Min()
+{
+	return ar[min];
+}
+
+int GetSize()
+{
+	return top;
+}
+
+int Capacity()
+{
+	return len;
+}
+
+bool IsFull()
+{
+	return (top == len) ? true : false;
+}
 void Print()
 {
-	if(top-1==0)
+	if(top==0)
 		cout<<"empty"<<endl;
 
-	while(--top>=0)
+	int t = top;
+
+	while(--t >=0)
 	{
-		cout<<ar[top]<<endl;
+		cout<<ar[t]<<" ";
 	}
+	cout<<endl;
 }
 private:
 	int * ar;
 	int len;
 	int top;
+	int min;//assuming stack contains all positive numbers
+};
+
+class SetOfStacks
+{
+	public:
+		SetOfStacks(int len)
+		{
+			stackSize = len;
+			stackNo = 0;
+			stacks.push_back(Stack(stackSize));
+		}
+
+		void Push(int x)
+		{
+			if(stacks[stackNo].IsFull())
+			{
+				stacks.push_back(Stack(stackSize));
+				stackNo++;
+			}
+
+			stacks[stackNo].Push(x);
+		}
+
+		int Pop()
+		{
+			if(stackNo == -1)
+				return -1;
+
+			if(stacks[stackNo].GetSize() == 0)
+				stackNo--;
+
+			if(stacks[stackNo].GetSize() -1 == 0)
+				return stacks[stackNo--].Pop();
+
+			return stacks[stackNo].Pop();
+		}
+
+		int PopAt(int stackNum)
+		{
+			if(stackNo == -1)
+				return -1;
+
+			if(stackNum > stackNo)
+				return -1;
+
+			return stacks[stackNum].Pop();
+		}
+		void Print()
+		{
+			int stNo = stackNo;
+
+			while(stNo >= 0)
+			{	
+				cout<<"StackNo : "<<stNo<<endl;
+				stacks[stNo].Print();
+				stNo--;
+			}
+		}
+	private:
+		vector<Stack> stacks;
+		int stackNo;
+		int stackSize;
 };
 
 int main(void)
 {
-	Stack * stck = new Stack(100);
+	SetOfStacks  stck(10);
 
-	for(int i=0; i<20; i++)
-		stck->Push(i);
-	stck->Pop();
-	stck->Print();
-	
+	for(int i=0; i<100; i++)
+		stck.Push(i);
+
+	stck.Print();
+
+	for(int i=0; i<5; i++)
+		cout<<"Element popped :"<<stck.PopAt(3)<<endl;
+
+	stck.Print();
+
+	// stck->Push(-7);
+	// //stck->Pop();
+	// stck->Print();
+	// cout<<"MIN: "<<stck->Min()<<endl;
+	// cout<<"Popped eleement: "<<stck->Pop()<<endl;
+	// stck->Print();
+	// cout<<"MIN: "<<stck->Min()<<endl;
 	return 0;
 }
